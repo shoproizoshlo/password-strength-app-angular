37a683ececaad16bbae634a85bc43ffa1254d51d
shoproizoshlo <iamsusunny@gmail.com>
2023-07-28 13:21:03 +0200

    Created a field for entering a password, under the field add 3 sections which will show the strength of the password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import this line
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PasswordStrengthIndicatorComponent } from './password-strength-indicator/password-strength-indicator.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PasswordStrengthIndicatorComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PasswordStrengthIndicatorComponent],
+  imports: [BrowserModule, FormsModule], // Add FormsModule here
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/password-strength-indicator/password-strength-indicator.component.ts
+++ src/app/password-strength-indicator/password-strength-indicator.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-password-strength-indicator',
-  templateUrl: './password-strength-indicator.component.html',
-  styleUrls: ['./password-strength-indicator.component.css']
+  template: `
+    <div class="password-strength-indicator">
+      <input
+        type="password"
+        [(ngModel)]="password"
+        (ngModelChange)="checkPasswordStrength()"
+        placeholder="Enter password"
+      />
+
+      <div class="strength-meter">
+        <div
+          class="strength-bar"
+          [ngStyle]="{ 'width.%': strengthPercentage }"
+        ></div>
+      </div>
+
+      <div class="strength-label">{{ strengthLabel }}</div>
+    </div>
+  `,
+  styles: [
+    `
+      .password-strength-indicator {
+        margin-top: 10px;
+      }
+
+      .strength-meter {
+        height: 10px;
+        background-color: #f1f1f1;
+        border-radius: 5px;
+        overflow: hidden;
+      }
+
+      .strength-bar {
+        height: 100%;
+        background-color: #4caf50;
+      }
+
+      .strength-label {
+        margin-top: 5px;
+        font-size: 12px;
+      }
+    `,
+  ],
 })
 export class PasswordStrengthIndicatorComponent {
+  @Input() password: string = '';
+  strengthPercentage: number = 0;
+  strengthLabel: string = '';
 
+  checkPasswordStrength() {
+    const lengthScore = this.password.length / 10;
+    const uppercaseScore = this.password.match(/[A-Z]/) ? 0.3 : 0;
+    const lowercaseScore = this.password.match(/[a-z]/) ? 0.3 : 0;
+    const numberScore = this.password.match(/\d/) ? 0.2 : 0;
+    const specialCharScore = this.password.match(
+      /[!@#$%^&*()_+{}\[\]:;<>,.?~\-=/\\]/
+    )
+      ? 0.2
+      : 0;
+
+    this.strengthPercentage =
+      (lengthScore +
+        uppercaseScore +
+        lowercaseScore +
+        numberScore +
+        specialCharScore) *
+      100;
+
+    if (this.strengthPercentage <= 25) {
+      this.strengthLabel = 'Weak';
+    } else if (this.strengthPercentage <= 50) {
+      this.strengthLabel = 'Moderate';
+    } else if (this.strengthPercentage <= 75) {
+      this.strengthLabel = 'Strong';
+    } else {
+      this.strengthLabel = 'Very Strong';
+    }
+  }
 }
